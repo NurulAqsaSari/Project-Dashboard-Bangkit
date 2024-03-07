86a7dcab114fbef680a53b23a5ee65e09c05d5e9
NurulAqsaSari <101948965+NurulAqsaSari@users.noreply.github.com>
2024-03-07 19:45:43 +0700

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load the data
-day = pd.read_csv('main_data.csv')
+day = pd.read_csv('https://raw.githubusercontent.com/NurulAqsaSari/Project-Dashboard-Bangkit/main/data/day.csv')
 
 # Streamlit app
 st.set_page_config(page_title="Bikecycle Rental")
